4ce019fb5b2973d21d528f8ccbc1ba1618bfb2bc
AndreyGrek12 <andreygrek12@yandex.ru>
2022-05-11 15:06:54 +0300

    U7 Task Update

--- src/test/resources/StartGrid.bat
+++ src/test/resources/StartGrid.bat
@@ -1,2 +1,2 @@
 start cmd /C java -jar selenium-server-4.1.4.jar hub
-start cmd /C java -jar selenium-server-4.1.4.jar node --config NodeSettings.toml
+java -jar selenium-server-4.1.4.jar node --config NodeSettings.toml
