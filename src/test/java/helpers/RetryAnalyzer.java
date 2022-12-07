936ff8d7a5ecbcebd68bc358ba625a6bf35d1d46
AndreyGrek12 <andreygrek12@yandex.ru>
2022-12-07 22:14:06 +0300

    some fixes

--- src/test/java/helpers/RetryAnalyzer.java
+++ src/test/java/helpers/RetryAnalyzer.java
@@ -6,7 +6,7 @@ import org.testng.ITestResult;
 public class RetryAnalyzer implements IRetryAnalyzer {
 
     private int retryCount = 1;
-    private static final int maxRetryCount = 1;
+    private static final int maxRetryCount = 2;
 
     @Override
     public boolean retry(ITestResult result) {
